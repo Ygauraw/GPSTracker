1d6c0402b0143716ba50ccbea501b95d13e6313a
Sergey <sergey.dvornikov@gmail.com>
2013-04-05 15:31:26 +0400

    Response generator & expanded LoginData cass

--- src/ru/cyfn/GPS/GPSTrackingServer.java
+++ src/ru/cyfn/GPS/GPSTrackingServer.java
@@ -9,7 +9,6 @@ public class GPSTrackingServer {
 
 	public void go() {
 		clientSockets = new ArrayList<Socket>();
-
 		try {
 			ServerSocket serverSock = new ServerSocket(5000);
 			System.out.println("GPSTrackingServer is waiting for clients");
@@ -28,8 +27,6 @@ public class GPSTrackingServer {
 class ClientHandler implements Runnable {
 	private GPSDataReader in;
 	private Socket clientSocket;
-	char[] LOGIN_RESPONSE = {0x78, 0x78, 0x5, 0x1, 0x9, 0xAE, 0x36, 0xF, 0xD, 0xA};
-
 	
 	public ClientHandler(Socket socket) {
 		try {
@@ -45,7 +42,7 @@ class ClientHandler implements Runnable {
 		GPSDataPackage packet;
 		try {
 			while (!clientSocket.isClosed() && (packet = in.readGPSData()) != null) {
-				System.out.print("In: ");
+				System.out.print("In:  ");
 				printArray(packet.getRawContent());
 				respond(packet);
 			}
@@ -57,19 +54,14 @@ class ClientHandler implements Runnable {
 	
 	private void respond(GPSDataPackage packet) {
 		OutputStreamWriter out;
-		if(packet.getType() == 0x01) {
+		if(packet.getType() == GPSDataPackage.LOGIN_PKG || packet.getType() == GPSDataPackage.STATUS_PKG) {
+			GPSDataPackage response = packet.createResponse();
 			try {
 				out = new OutputStreamWriter(clientSocket.getOutputStream());
-				LOGIN_RESPONSE[4] = packet.getSerialNumber()[0];
-				LOGIN_RESPONSE[5] = packet.getSerialNumber()[1];
-				int crc = CRC.getCRC(Arrays.copyOfRange(LOGIN_RESPONSE, 2, 6));
-				LOGIN_RESPONSE[6] = (char)((crc & 0xFF00)>>>8);
-				LOGIN_RESPONSE[7] = (char)(crc & 0xFF);
-				out.write(LOGIN_RESPONSE);
+				out.write(response.getRawContent());
 				out.flush();
 				System.out.print("Out: ");
-				printArray(LOGIN_RESPONSE);
-				//out.close();
+				printArray(response.getRawContent());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -81,18 +73,6 @@ class ClientHandler implements Runnable {
 			System.out.print(String.format("0x%X,", (int) array[i]));
 		}
 		System.out.println("");
-	}
-}
-
-enum GPSDataPackageType {
-	LOGIN(0x01);
-	
-	private int typeCode;
-	GPSDataPackageType(int type) {
-		typeCode = type;
-	}
-	public int getTypeCode() {
-		return typeCode;
 	}
 }
 
@@ -109,48 +89,61 @@ class GPSDataPackage {
 	private char[] rawContent;
 	private GPSDataContent dataContent;
 	
-	// !!! delete this method after implementing createResponse()
-	public char[] getSerialNumber() {
-		return Arrays.copyOf(serialNumber, serialNumber.length);
-	}
-	
 	public int getType() {
 		return packageType;
 	}
-	
 	public boolean isValid() {
 		return validCRC;
 	}
-	
 	public char[] getRawContent() {
 		return Arrays.copyOf(rawContent, rawContent.length);
 	}
-	
+	public LinkedHashMap<String, String> getDataContent() {
+		return dataContent.getDataContent();
+	}
 	public GPSDataPackage(char[] data) {
 		rawContent = Arrays.copyOf(data, data.length);
+		init();
+	}
+	
+	private GPSDataPackage(int type, char[] serialNum) {
+		char[] result = new char[10];
+		result[0] = 0x78;
+		result[1] = 0x78;
+		result[2] = 0x5;
+		result[3] = (char)type;
+		result[4] = serialNum[0];
+		result[5] = serialNum[1];
+		int crc = CRC.getCRC(Arrays.copyOfRange(result, 2, 6));
+		result[6] = (char)((crc & 0xFF00)>>>8);
+		result[7] = (char)(crc & 0xFF);
+		result[8] = 0xD;
+		result[9] = 0xA;
+		rawContent = result;
+		init();
+	}
+	
+	private void init() {
 		validCRC = isPacketValid(rawContent);
-		if(validCRC) {
-			packageType = rawContent[3];
-			serialNumber[0] = rawContent[rawContent.length-6];
-			serialNumber[1] = rawContent[rawContent.length-5];
-			dataContent = constructDataContent(packageType, Arrays.copyOfRange(rawContent, 4, rawContent.length - 6));
-		}
+		packageType = rawContent[3];
+		serialNumber[0] = rawContent[rawContent.length-6];
+		serialNumber[1] = rawContent[rawContent.length-5];
+		dataContent = constructDataContent(packageType, Arrays.copyOfRange(rawContent, 4, rawContent.length - 6));
 	}
 	
 	private GPSDataContent constructDataContent(int type, char[] data) {
 		GPSDataContent result = null;
-		
-		switch (type) {
-		case LOGIN_PKG:
-			result = new LoginData(data);
-			break;
-		}
+		if(data.length == 0) return null;
+			switch (type) {
+			case LOGIN_PKG:
+				result = new LoginData(data);
+				break;
+			}
 		return result;
 	}
 	
 	public GPSDataPackage createResponse() {
-		// construct and return appropriate response for current package type
-		return null;
+		return new GPSDataPackage(packageType, serialNumber);
 	}
 	
 	private boolean isPacketValid(char[] data) {
@@ -163,22 +156,41 @@ class GPSDataPackage {
 
 interface GPSDataContent {
 	public char[] toRawChars();
-	public String toString();
+	public LinkedHashMap<String, String> getDataContent();
 }
+
+/*
+ * LoginData
+ * Device ID - 8 chars
+ * Type identity code - 2 chars
+ */
 
 class LoginData implements GPSDataContent {
 	private char[] deviceID;
 	private char[] typeIdentityCode;
 	
 	public LoginData(char[] rawData) {
-		
+		deviceID = Arrays.copyOfRange(rawData, 0, 8);
+		typeIdentityCode = Arrays.copyOfRange(rawData, 8, 10);
 	}
 	
 	public char[] toRawChars() {
 		return null;
 	}
 	
-	public String toString() {
-		return null;
+	private String deviceIdToString() {
+		String result = "";
+		
+		for (int i = 0; i < deviceID.length; i++) {
+			result = result + String.format("%x", deviceID[i]);
+		}
+		return result.substring(1);
+	}
+	
+	public LinkedHashMap<String, String> getDataContent() {
+		LinkedHashMap<String, String> result = new LinkedHashMap<String, String>();
+		result.put("Device ID", deviceIdToString());
+		result.put("Type identity code", String.format("0x%X,0x%X",typeIdentityCode[0],typeIdentityCode[1]));
+		return result;
 	}	
 }
