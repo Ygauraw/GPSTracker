3e85719e4698d2cfcd35267006fe5f0dbfb66685
Sergey <sergey.dvornikov@gmail.com>
2013-04-08 20:57:41 +0400

    removed debugging method

--- src/ru/cyfn/GPS/ServerTest.java
+++ src/ru/cyfn/GPS/ServerTest.java
@@ -1,21 +1,11 @@
 package ru.cyfn.GPS;
 
-import java.util.*;
+import ru.cyfn.GPSTrackingServer.*;
 
 public class ServerTest {
 
 	public static void main(String[] args) {
-		//CRCTest();
 		new GPSTrackingServer().go();
-	}
-	
-	private static void CRCTest() {
-		
-		System.out.println(String.format("%tY-%<tm-%<td %<tH:%<tM:%<tS",new Date()));
-		  byte[] array = { 
-				  			0x5,0x13,0xE,0xE
-		  				 };
-		System.out.println(String.format("Calculated CRC value: 0x%X", CRC.getCRC_ITU(array)));
 	}
 }
 
