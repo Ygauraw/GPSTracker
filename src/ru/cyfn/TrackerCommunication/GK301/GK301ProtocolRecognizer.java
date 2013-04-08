0a6d4977133b90092cae182ad4faaea107c8862d
Sergey <sergey.dvornikov@gmail.com>
2013-04-09 00:22:03 +0400

    Sub for GK301 protocol recognizer

---
+++ src/ru/cyfn/TrackerCommunication/GK301/GK301ProtocolRecognizer.java
@@ -0,0 +1,23 @@
+package ru.cyfn.TrackerCommunication.GK301;
+
+import ru.cyfn.TrackerCommunication.GPSDataPackage;
+import ru.cyfn.TrackerCommunication.ProtocolRecognizer;
+
+public class GK301ProtocolRecognizer extends ProtocolRecognizer {
+
+	public GPSDataPackage createDataPackage(byte[] rawData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int testRawBytes(byte[] rawData) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getProtocolName() {
+		return "GK301 GPS Kids’ Phone Communication Protocol";
+		
+	}
+
+}
